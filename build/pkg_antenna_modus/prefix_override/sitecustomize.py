fa877c2d5d207e32a4abcf11db9a540920b2e76b
Berkant548 <24165794@student.hhs.nl>
2025-06-08 21:10:54 +0200

    Updated antenna and launch packages, cleaned build artifacts

--- build/pkg_antenna_modus/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/berko-vm/ros2_ws/install/pkg_antenna_modus'
--- build/pkg_img/share/pkg_img/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/berko-vm/ros2_ws/build/pkg_img"
